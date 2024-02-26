0a275ddb9cb8e4c75fed6b9188714e3525f9af26
Andrey Mendonça <andrey.mmendonca@gmail.com>
2024-02-26 19:45:57 -0300

    Classe Tarefas implementada

--- src/main/java/com/projetoAndrey/entities/Tarefas.java
+++ src/main/java/com/projetoAndrey/entities/Tarefas.java
@@ -1,5 +1,100 @@
 package com.projetoAndrey.entities;
 
-public class Tarefas {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_tarefas")
+public class Tarefas implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String descricao;
+	private LocalDate dataCadastro;
+	private LocalDate dataTermino;
+	private String status;
+	private Boolean concluida;
+	
+	public Tarefas() {};
+
+	public Tarefas(Long id, String name, String descricao, LocalDate dataTermino, String status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.descricao = descricao;
+		this.dataTermino = dataTermino;
+		this.concluida = false;
+		setDataCadastro();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro() {
+		this.dataCadastro = LocalDate.now();
+	}
+
+	public LocalDate getDataTermino() {
+		return dataTermino;
+	}
+
+	public void setDataTermino(LocalDate dataTermino) {
+		this.dataTermino = dataTermino;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Boolean getConcluida() {
+		return concluida;
+	}
+
+	public void setConcluida(Boolean concluida) {
+		this.concluida = concluida;
+	}
+	
+	
+	
+	
 
 }
